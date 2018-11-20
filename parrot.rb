21f917563ff367cfea26cf0f81e546a621b639c0
Aglae Verduzco <aglae.cv@gmail.com>
2018-11-20 01:52:19 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(*p, phrase = "Pretty birdie!")
+def parrot(*p, phrase="Pretty birdie!")
   puts "Squawk!"
   return "Squawk!"
 end
